659837ffdc29946e62c72d543d5a4e75fe631c2b
dgketchum <dgketchum@gmail.com>
2018-06-28 10:38:47 -0700

    added names of files, stuck on invalid shapefile

--- build_qgs.py
+++ build_qgs.py
@@ -14,48 +14,56 @@
 # limitations under the License.
 # ===============================================================================
 
-# PYTHONPATH = / < qgispath > / share / qgis / python
 import os
 
 home = os.path.expanduser('~')
-path = os.path.join(home, 'miniconda2', 'envs', 'qgis', 'share', 'qgis', 'python')
+PATH = os.path.join(home, 'miniconda2', 'envs', 'qgis', 'share', 'qgis', 'python')
 import sys
 
-sys.path.append(path)
+sys.path.append(PATH)
 
-from xml.dom.minidom import Document
-import string
 import os
-import sys
-from qgis.core import *
-from qgis.gui import *
-from PyQt4.QtCore import *
-from PyQt4.QtGui import QApplication
-from PyQt4.QtXml import *
+from qgis.core import QgsApplication, QgsProject, QgsMapLayerRegistry
+from qgis.core import QgsRasterLayer, QgsVectorLayer
+from PyQt4.QtCore import QFileInfo
 
 
 def make_qgs(template, input):
 
-    QGISAPP = QgsApplication(sys.argv, True)
-    QgsApplication.setPrefixPath(os.path.dirname(input), True)
-    QgsApplication.initQgis()
-    QgsProject.instance().setFileName(template)
-    print QgsProject.instance().fileName()
+    shapes = {'cold': os.path.join(input, 'PIXELS', 'cold.shp'),
+           'hot': os.path.join(input, 'PIXELS', 'hot.shp')}
 
-    for file1 in os.listdir(r"C:\myprojects\world"):
-        if file1.endswith('.shp'):
-            layer = QgsVectorLayer(r"C:\myprojects\world" + r"\\" + file1, file1, "ogr")
-            print file1
-            print layer.isValid()
-            # Add layer to the registry
-            QgsMapLayerRegistry.instance().addMapLayer(layer)
+    rasters = {'hot_pixels': os.path.join(input, 'PIXEL_REGIONS', 'hot_pixel_suggestion.img'),
+           'cold_pixels': os.path.join(input, 'PIXEL_REGIONS', 'cold_pixel_suggestion.img'),
+           'region_mask': os.path.join(input, 'PIXEL_REGIONS', 'region_mask.img'),
+           'ndvi': os.path.join(input, 'INDICES', 'ndvi_toa.img'),
+           'ts': os.path.join(input, 'ts.img'),
+           'albedo': os.path.join(input, 'albedo_at_sur.img'),
+           'et_rf': os.path.join(input, 'ETRF', 'et_rf.img')}
 
-    QgsProject.instance().write()
-    QgsApplication.exitQgis()
+    QgsApplication.setPrefixPath(PATH, True)
+    qgs = QgsApplication([], False)
+    qgs.initQgis()
 
-    add_Layers()
+    # start a project
+    project = QgsProject.instance()
+    project_path = QFileInfo(os.path.join(input, 'calbration_map.qgs'))
+    for key, path in rasters.items():
+        layer = QgsRasterLayer(path)
+        if not layer.isValid():
+            print("file {} is not a valid raster file".format(path))
 
-    return None
+        QgsMapLayerRegistry.instance().addMapLayer(layer)
+
+    for key, path in shapes.items():
+        layer = QgsVectorLayer(path)
+        if not layer.isValid():
+            print("file {} is not a valid vector file".format(path))
+
+        QgsMapLayerRegistry.instance().addMapLayer(layer)
+
+    project.write(project_path)
+    qgs.exitQgis()
 
 
 if __name__ == '__main__':
