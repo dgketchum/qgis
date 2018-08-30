e8dac8cb3d404020332c695329fba44e2a5b567d
dgketchum <dgketchum@gmail.com>
2018-08-29 19:27:35 -0600

    got it!

--- build_map.py
+++ build_map.py
@@ -18,23 +18,31 @@ import os
 import lxml.etree as et
 
 
-def modify_qgs(template, input):
+def modify_qgs(template, input_loc):
+    sources = {'cold': os.path.join(input_loc, 'PIXELS', 'cold.shp'),
+               'hot': os.path.join(input_loc, 'PIXELS', 'hot.shp'),
+               'hot_pixel_suggestion': os.path.join(input_loc, 'PIXEL_REGIONS',
+                                                    'hot_pixel_suggestion.img'),
+               'cold_pixel_suggestion': os.path.join(input_loc, 'PIXEL_REGIONS',
+                                                     'cold_pixel_suggestion.img'),
+               'region_mask': os.path.join(input_loc, 'PIXEL_REGIONS', 'region_mask.img'),
+               'ndvi_toa': os.path.join(input_loc, 'INDICES', 'ndvi_toa.img'),
+               'ts': os.path.join(input_loc, 'ts.img'),
+               'albedo_at_sur': os.path.join(input_loc, 'albedo_at_sur.img'),
+               'et_rf': os.path.join(input_loc, 'ETRF', 'et_rf.img')}
+
+    rep_rt = '/home/dgketchum/IrrigationGIS/tests/qgis/LC08_041027_20150807/ts.img'
+    xp = '//qgis/layer-tree-group/layer-tree-layer'
     with open(template) as f:
         tree = et.parse(f)
         root = tree.getroot()
+        for key, val in sources.items():
+            item = tree.xpath("//qgis/layer-tree-group/layer-tree-layer[@name='{}']".format(key))
+            for s in item:
+                s.attrib['source'] = val
 
-        for elem in root.getiterator():
-            try:
-                elem.text = elem.text.replace(
-                    '/home/dgketchum/IrrigationGIS/tests/qgis/LC08_041027_20150807/ts.img',
-                    '/home/dgketchum/IrrigationGIS/tests/qgis/LC08_041027_20160825/ts.img')
-
-            except AttributeError:
-                pass
-
-    output = os.path.join(input, '{}_calbration_map.qgs'.format(os.path.basename(input)))
-
-    tree.write(output, xml_declaration=True, method='xml', encoding="utf8")
+    output = os.path.join(input_loc, '{}_calbration_map.qgs'.format(os.path.basename(input_loc)))
+    tree.write(output, xml_declaration=True, method='xml', encoding="utf8", pretty_print=True)
 
 
 if __name__ == '__main__':
@@ -45,7 +53,6 @@ if __name__ == '__main__':
     for r, d, f in os.walk(path):
         if 'ETRF' in d:
             targets.append(r)
-            print(r, d, f)
             modify_qgs(t, r)
 
 # ========================= EOF ====================================================================
